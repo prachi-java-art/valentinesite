1ae3f37224e2cdda1717a634023adb3c31604142
Diksha Joshi <dikshajoshi1060@gmail.com>
2026-01-19 01:37:57 +0530

    First Valentine site 💌

---
+++ src/main/java/com/love/valentinesite/ValentinesiteApplication.java
@@ -0,0 +1,13 @@
+package com.love.valentinesite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValentinesiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValentinesiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/love/valentinesite/ValentinesiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.love.valentinesite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValentinesiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
